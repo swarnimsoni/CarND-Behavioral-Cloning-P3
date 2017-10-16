3e973d2bff5aca2e656b869506abc356dd1ef90d
SS <swarnims.iitm@gmail.com>
2017-10-16 05:48:53 +0530

    Crop and resize the images sent from simulator to the model so that the size fed to simulator is 66x200x3 (exactly as Nvidia's model)

--- drive.py
+++ drive.py
@@ -61,9 +61,9 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         # crop the image
-
+        image = image.crop((0, 51, 320, 135))
         # resize the image to 66x200
-
+        image = image.resize((200,66), Image.ANTIALIAS)
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
